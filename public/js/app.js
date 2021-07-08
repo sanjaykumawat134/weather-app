554227789f3856167e5058662d480fe29a01f383
sanjay kumawat <sanjaykumawat134@gmail.com>
2021-07-08 17:12:00 +0530

    final changes"

--- public/js/app.js
+++ public/js/app.js
@@ -3,19 +3,25 @@ console.log("client side javascript");
 const form = document.getElementsByTagName("form")[0];
 console.log("form", form);
 const search = form.querySelector("input");
+const msg1 = document.querySelector("#msg1");
+const msg2 = document.querySelector("#msg2");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const value = search.value;
+  msg1.textContent = "Loading...";
+  msg2.textContent = "";
   if (value == "") {
     alert("please enter a value");
   } else {
     fetch("http://localhost:3000/weather?address=" + value).then((response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log(data.error);
+          msg1.textContent = data.error;
         } else {
-          console.log(data.location);
-          console.log(data.forecast);
+          // console.log(data.location);
+          // console.log(data.forecast);
+          msg1.textContent = data.location;
+          msg2.textContent = data.forecast;
         }
       });
     });
